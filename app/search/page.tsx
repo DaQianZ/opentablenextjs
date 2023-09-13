c383af1587b2992eadbd033a29def47794a5d066
qian.zhang2 <qian.zhang2@thoughtworks.com>
2023-09-13 17:05:11 +0800

    add fetch api for search restaurant by city

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,18 +1,57 @@
+import { PrismaClient } from "@prisma/client";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-export default function Search() {
-    return (
-        <>
-            <Header />
-            <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-                <SearchSideBar />
-                <div className="w-5/6">
-                    <RestaurantCard />
-                </div>
-            </div>
-        </>
+const prisma = new PrismaClient();
 
-    )
+const fetchRestaurantsByCity = (city: string | undefined) => {
+  const select = {
+    id: true,
+    name: true,
+    main_img: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+export default async function Search({
+  searchParams,
+}: {
+  searchParams: { city: string };
+}) {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+  return (
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <SearchSideBar />
+        <div className="w-5/6">
+          {restaurants.length ? (
+            <>
+              {restaurants.map((restaurant) => (
+                <RestaurantCard restaurant={restaurant} />
+              ))}
+            </>
+          ) : (
+            <p>Sorry, we found no restaurants in this area</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
