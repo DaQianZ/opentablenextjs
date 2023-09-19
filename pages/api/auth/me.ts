4f516b31685e5ddb34a4b8c2f0ed74945780684d
qian.zhang2 <qian.zhang2@thoughtworks.com>
2023-09-19 16:19:17 +0800

    add middleware

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -10,27 +10,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const bearerToken = req.headers["authorization"] as string;
-  if (!bearerToken) {
-    return res.status(401).json({
-      errorMessage: "Unauthorized request (no bearer token)",
-    });
-  }
-
   const token = bearerToken.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({
-      errorMessage: "Unauthorized request (no token)",
-    });
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    return res.status(401).json({
-      errorMessage: "Unauthorized request (token invalid)",
-    });
-  }
 
   const payload = jwt.decode(token) as { email: string };
 
