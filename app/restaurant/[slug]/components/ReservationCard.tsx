f52652b49967c627822ffc0386592caa06e7ce24
qian.zhang2 <qian.zhang2@thoughtworks.com>
2023-11-16 11:34:58 +0800

    add partySize

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,36 +1,55 @@
+"use client";
+import { partySize } from "../../../../data";
+import DatePicker from "react-datepicker";
+import { useState } from "react";
+
 export default function ReservationCard() {
-    return (
-        <div className="fixed w-[15%] bg-white rounded p-3 shadow">
-            <div className="text-center border-b pb-2 font-bold">
-                <h4 className="mr-7 text-lg">Make a Reservation</h4>
-            </div>
-            <div className="my-3 flex flex-col">
-                <label htmlFor="">Party size</label>
-                <select name="" className="py-3 border-b font-light" id="">
-                    <option value="">1 person</option>
-                    <option value="">2 people</option>
-                </select>
-            </div>
-            <div className="flex justify-between">
-                <div className="flex flex-col w-[48%]">
-                    <label htmlFor="">Date</label>
-                    <input type="text" className="py-3 border-b font-light w-28" />
-                </div>
-                <div className="flex flex-col w-[48%]">
-                    <label htmlFor="">Time</label>
-                    <select name="" id="" className="py-3 border-b font-light">
-                        <option value="">7:30 AM</option>
-                        <option value="">9:30 AM</option>
-                    </select>
-                </div>
-            </div>
-            <div className="mt-5">
-                <button
-                    className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
-                >
-                    Find a Time
-                </button>
-            </div>
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+
+  const handleChangeDate = (date: Date | null) => {
+    if (date) {
+      return setSelectedDate(date);
+    }
+    return setSelectedDate(null);
+  };
+
+  return (
+    <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+      <div className="text-center border-b pb-2 font-bold">
+        <h4 className="mr-7 text-lg">Make a Reservation</h4>
+      </div>
+      <div className="my-3 flex flex-col">
+        <label htmlFor="">Party size</label>
+        <select name="" className="py-3 border-b font-light" id="">
+          {partySize.map((size) => (
+            <option value={size.value}>{size.label}</option>
+          ))}
+        </select>
+      </div>
+      <div className="flex justify-between">
+        <div className="flex flex-col w-[48%]">
+          <label htmlFor="">Date</label>
+          <DatePicker
+            selected={selectedDate}
+            onChange={handleChangeDate}
+            className="py-3 border-b font-light text-reg w-28"
+            dateFormat={"MMMM d"}
+            wrapperClassName="w-[48%]"
+          />
         </div>
-    )
+        <div className="flex flex-col w-[48%]">
+          <label htmlFor="">Time</label>
+          <select name="" id="" className="py-3 border-b font-light">
+            <option value="">7:30 AM</option>
+            <option value="">9:30 AM</option>
+          </select>
+        </div>
+      </div>
+      <div className="mt-5">
+        <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
+          Find a Time
+        </button>
+      </div>
+    </div>
+  );
 }
